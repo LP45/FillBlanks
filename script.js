a1fd04ae6b0c12cd3e863e43e3ed65a1c0611be5
Dan Lo <nflwwe71@gmail.com>
2020-03-26 19:06:14 -0400

    drafted version

---
+++ script.js
@@ -0,0 +1,75 @@
+
+// let coffee = "French";
+
+// let flavor = "Vanilla";
+
+// let order = coffee.concat(flavor);
+
+// console.log(order);
+
+// //I have 3 coffees.
+
+// //I also have 10 chais.
+
+// //I have  a total of 13 drinks total.
+
+
+// coffee = 3;
+
+// let chai = 10;
+
+// console.log("I have" +coffee + "coffe");
+
+// console.log("I also have" +chai+"chais");
+
+// console.log("I have a total of" +Number(chai+coffee) +"drinks totoal");
+
+
+function myFunction() {
+
+    let str = "Can you guess what is the index of 42 is? I am sure you will be surprised!";
+  
+    let answer = str.indexOf("42");
+  
+  console.log(answer);
+  
+  }
+  
+
+  function myFunction() {
+
+    let str = "Can you guess how long this string is? I am sure you will be surprised! (Hint:It is not 42.)";
+  
+    let answer = str.length
+  
+  alert(answer);
+  
+  };
+
+  function myFunction() {
+
+    var str = "         SOCIAL DISTANCING!!!                    ";
+  
+    alert(str);
+  
+  };
+
+  function myFunction() {
+
+    let str = "social distancing!!";
+  
+    let res = str.toUpperCase;
+  
+    console.log(res);
+  
+  };
+
+  function myFunction() {
+
+    let str = "QuiEt VoICeS...PLEASE";
+  
+    let res = str.toLowerCase;
+  
+    console.log(res);
+  
+  }
